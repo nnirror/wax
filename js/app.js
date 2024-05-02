8d18ee5180e70431d47156139bfd3821086b45ea
mjcella <michael.j.cella@gmail.com>
2024-05-02 12:52:25 -0400

    navbar responsive style improvements and microphone example device positioning in workspace

--- js/app.js
+++ js/app.js
@@ -274,7 +274,7 @@ firstOption.selected = true;
 dropdown.appendChild(firstOption);
 
 // add an option for each file
-['amplitude_modulation.zip', 'audio_file_playback.zip', 'fm.zip', 'hello_world_tri.zip', 'patterns_with_facet.zip', 'regen_example.zip', 'microphone_input.zip'].forEach(function(file) {
+['amplitude_modulation.zip', 'audio_file_playback.zip', 'fm.zip', 'hello_world_tri.zip', 'patterns_with_facet.zip', 'regen_example.zip', 'microphone.zip'].forEach(function(file) {
     var option = document.createElement('option');
     option.value = file;
     // remove underscores and '.zip' from the display text
@@ -1366,6 +1366,7 @@ async function loadExampleFile(filePath) {
     let patcherState = await zip.file(jsonFileName).async('string');
     deviceStates = JSON.parse(patcherState);
     await reconstructDevicesAndConnections(deviceStates, zip, false);
+    deselectAllNodes();
 }
 
 async function reconstructDevicesAndConnections(deviceStates, zip, reconstructFromDuplicateCommand = false) {
